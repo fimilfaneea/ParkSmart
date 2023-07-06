3ee039b5079df023b231a21121ef2ed0063fde43
Jeremy Varughese <jeremyvarughese3@gmail.com>
2023-07-06 22:06:35 +0530

    fixed ticket viewing issue

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       home: const LoginPage(),
       
       routes: {
-       "home": (context) => HomePage(),
+       "home": (context) => const HomePage(),
       },
     );
   }
